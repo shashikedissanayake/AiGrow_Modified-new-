0ff135bcd4981f62f5f0638a0c897b6c3ea53394
shashikedissanayake <shashikedissanayake@gmail.com>
2017-12-18 10:18:10 +0530

    device add page included

---
+++ AiGrow.Portal/Dashboards/Admin/AdminAddDevice.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AiGrow.Portal.Dashboards.Admin
+{
+    public partial class AdminAddDevice : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
